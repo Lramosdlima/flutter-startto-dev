06a9904b543f83561d3300b9be1358029871d28e
Leonardo R. de Lima <lramoslima97@gmail.com>
2024-10-05 15:47:29 -0300

    feat: :sparkles: explore textfields decorations

--- todo_list/lib/pages/todo_list_page.dart
+++ todo_list/lib/pages/todo_list_page.dart
@@ -5,6 +5,54 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+          child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Exemplos de TextFields', style: TextStyle(fontSize: 24)),
+            SizedBox(height: 32),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Email",
+                hintText: "exemplo@email.com",
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Preço",
+                prefixText: "R\$ ",
+                hintText: "00.00",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Peso",
+                suffixText: "kg",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Data",
+                suffixIcon: Icon(Icons.calendar_today),
+              ),
+              keyboardType: TextInputType.datetime,
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
