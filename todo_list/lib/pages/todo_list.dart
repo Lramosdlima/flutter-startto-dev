063701d679ca6b356582e670c35919cf8f9c0609
Leonardo R. de Lima <lramoslima97@gmail.com>
2024-10-05 17:06:36 -0300

    feat: :sparkles: add new row and use expanded to get space

--- todo_list/lib/pages/todo_list.dart
+++ todo_list/lib/pages/todo_list.dart
@@ -9,31 +9,52 @@ class TodoListPage extends StatelessWidget {
       body: Center(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Row(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            const Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Adicione uma tarefa",
-                  hintText: "Exemplo: Estudar Flutter",
-                  hintStyle: TextStyle(color: Colors.grey),
+            Row(
+              children: [
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Adicione uma tarefa",
+                      hintText: "Exemplo: Estudar Flutter",
+                      hintStyle: TextStyle(color: Colors.grey),
+                    ),
+                  ),
                 ),
-              ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.all(14),
+                    shape: const CircleBorder(),
+                  ),
+                  onPressed: () {},
+                  child: const Icon(
+                    Icons.add,
+                    size: 30,
+                  ),
+                )
+              ],
             ),
-            const SizedBox(width: 8),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).primaryColor,
-                foregroundColor: Colors.white,
-                padding: const EdgeInsets.all(14),
-                shape: const CircleBorder(),
-              ),
-              onPressed: () {},
-              child: const Icon(
-                Icons.add,
-                size: 30,
-              ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                const Expanded(
+                    child: Text('Você possui 0 tarefa(s) pendente(s).')),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.all(14),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Limpar tudo'))
+              ],
             )
           ],
         ),
