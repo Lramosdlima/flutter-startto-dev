95b4a00e0b08b38e3f758171f68a16ca7f0862aa
Leonardo R. de Lima <lramoslima97@gmail.com>
2024-10-05 23:53:37 -0300

    feat: :sparkles: add method to load tasks

--- todo_list/lib/repositories/todo_repository.dart
+++ todo_list/lib/repositories/todo_repository.dart
@@ -3,15 +3,32 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_list/models/task.dart';
 
-class TaskRepository {
-  TaskRepository() {
-    SharedPreferences.getInstance().then((value) => _sharedPreferences = value);
-  }
+// Chave das tasks.
+const taskListKey = 'task_list';
 
+class TaskRepository {
   late SharedPreferences _sharedPreferences;
 
+  Future<List<Task>> loadTaskList() async {
+    // getInstance usa um padrão de singleton, para que apenas uma instância
+    // seja criada.
+    _sharedPreferences = await SharedPreferences.getInstance();
+
+    // Pega os dados do SharedPreferences pelo chave 'task_list'.
+    final String jsonString = _sharedPreferences.getString(taskListKey) ?? '[]';
+
+    // Converte o json para uma lista.
+    final List jsonDecoded = json.decode(jsonString) as List;
+
+    // Converte a lista de json para uma lista de Task.
+    return jsonDecoded.map((e) => Task.fromJson(e)).toList();
+  }
+
   void saveTaskList(List<Task> tasks) {
+    // Converte a lista de Task para uma lista de json.
     final String jsonString = json.encode(tasks);
-    _sharedPreferences.setString('tasks', jsonString);
+
+    // O json vai ser armazenado no SharedPreferences.
+    _sharedPreferences.setString(taskListKey, jsonString);
   }
 }
