65d3816bb8ade0e399affa7d7515d6a30acc7822
Leonardo R. de Lima <lramoslima97@gmail.com>
2024-10-05 23:20:54 -0300

    refactor: :recycle: add safearea on textfield page

--- todo_list/lib/pages/text_fields.dart
+++ todo_list/lib/pages/text_fields.dart
@@ -10,94 +10,95 @@ class TextFieldPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Exemplos de TextFields',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                )),
-            const SizedBox(height: 32),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Email",
-                hintText: "exemplo@email.com",
-                hintStyle: TextStyle(color: Colors.grey),
-              ),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: onChanged,
-              onSubmitted: onSubmitted,
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _priceController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Preço",
-                prefixText: "R\$ ",
-                hintText: "00.00",
-                hintStyle: TextStyle(color: Colors.grey),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _weightController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Peso",
-                suffixText: "kg",
-                hintText: "00.00",
-                hintStyle: TextStyle(color: Colors.grey),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _dateController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Data",
-                suffixIcon: Icon(Icons.calendar_today),
-                hintText: "00/00/0000",
-                hintStyle: TextStyle(color: Colors.grey),
-              ),
-              keyboardType: TextInputType.datetime,
-            ),
-            const SizedBox(height: 32),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    foregroundColor: Colors.white,
-                  ),
-                  onPressed: save,
-                  child: const Text('Salvar'),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+            child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Exemplos de TextFields',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  )),
+              const SizedBox(height: 32),
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Email",
+                  hintText: "exemplo@email.com",
+                  hintStyle: TextStyle(color: Colors.grey),
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    foregroundColor: Colors.white,
-                  ),
-                  onPressed: null,
-                  child: const Text('Botão Desabilitado'),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: onChanged,
+                onSubmitted: onSubmitted,
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                controller: _priceController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Preço",
+                  prefixText: "R\$ ",
+                  hintText: "00.00",
+                  hintStyle: TextStyle(color: Colors.grey),
                 ),
-              ],
-            ),
-          ],
-        ),
-      )),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                controller: _weightController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Peso",
+                  suffixText: "kg",
+                  hintText: "00.00",
+                  hintStyle: TextStyle(color: Colors.grey),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                controller: _dateController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Data",
+                  suffixIcon: Icon(Icons.calendar_today),
+                  hintText: "00/00/0000",
+                  hintStyle: TextStyle(color: Colors.grey),
+                ),
+                keyboardType: TextInputType.datetime,
+              ),
+              const SizedBox(height: 32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: save,
+                    child: const Text('Salvar'),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: null,
+                    child: const Text('Botão Desabilitado'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )),
+      ),
     );
   }
 
