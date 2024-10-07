fb100c4c25890af48d211ef55e6a2167063b3abb
Leonardo R. de Lima <lramoslima97@gmail.com>
2024-10-06 23:59:11 -0300

    feat: :sparkles: add calculate function

--- flutter_app/lib/pages/calculate_imc.dart
+++ flutter_app/lib/pages/calculate_imc.dart
@@ -10,10 +10,60 @@ class CalculateIMCPage extends StatefulWidget {
 }
 
 class _CalculateIMCPageState extends State<CalculateIMCPage> {
-  TextEditingController pesoController = TextEditingController();
-  TextEditingController alturaController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
 
-  final formKey = GlobalKey<FormState>();
+  String _infoText = 'Informe seus dados';
+
+  late Color infoTextColor = Theme.of(context).primaryColor;
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _resetFields() {
+    weightController.text = '';
+    heightController.text = '';
+    _formKey.currentState!.reset();
+    setState(() {
+      _infoText = 'Informe seus dados';
+      infoTextColor = Theme.of(context).primaryColor;
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _calculate() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      SystemChannels.textInput.invokeMethod('TextInput.hide');
+
+      setState(() {
+        double weight =
+            double.parse(weightController.text.replaceAll(',', '.'));
+        double height =
+            double.parse(heightController.text.replaceAll(',', '.'));
+        double imc = weight / (height * height);
+        if (imc < 18.6) {
+          _infoText = 'Abaixo do Peso (${imc.toStringAsPrecision(4)})';
+          infoTextColor = Colors.red;
+        } else if (imc >= 18.6 && imc < 24.9) {
+          _infoText = 'Peso Ideal (${imc.toStringAsPrecision(4)})';
+          infoTextColor = Colors.green;
+        } else if (imc >= 24.9 && imc < 29.9) {
+          _infoText = 'Levemente Acima do Peso (${imc.toStringAsPrecision(4)})';
+          infoTextColor = Colors.amber;
+        } else if (imc >= 29.9 && imc < 34.9) {
+          _infoText = 'Obesidade Grau I (${imc.toStringAsPrecision(4)})';
+          infoTextColor = Colors.orange;
+        } else if (imc >= 34.9 && imc < 39.9) {
+          _infoText = 'Obesidade Grau II (${imc.toStringAsPrecision(4)})';
+          infoTextColor = Colors.deepOrange;
+        } else if (imc >= 40) {
+          _infoText = 'Obesidade Grau III (${imc.toStringAsPrecision(4)})';
+          infoTextColor = Colors.red;
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +76,7 @@ class _CalculateIMCPageState extends State<CalculateIMCPage> {
         actions: [
           IconButton(
             icon: const Icon(Icons.refresh, color: Colors.white),
-            onPressed: () {},
+            onPressed: _resetFields,
           ),
         ],
       ),
@@ -34,7 +84,7 @@ class _CalculateIMCPageState extends State<CalculateIMCPage> {
           child: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
-          key: formKey,
+          key: _formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
@@ -48,15 +98,25 @@ class _CalculateIMCPageState extends State<CalculateIMCPage> {
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
-                      TextField(
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Este campo é obrigatório';
+                          }
+
+                          if (double.parse(value.replaceAll(',', '.')) <=
+                              30.0) {
+                            return 'Peso inválido';
+                          }
+                        },
                         keyboardType: TextInputType.number,
                         inputFormatters: [
                           TextInputMask(
-                            mask: '99,9',
+                            mask: '999,9',
                             reverse: false,
                           )
                         ],
-                        controller: pesoController,
+                        controller: weightController,
                         textAlign: TextAlign.center,
                         decoration: const InputDecoration(
                           suffixText: 'kg',
@@ -67,7 +127,20 @@ class _CalculateIMCPageState extends State<CalculateIMCPage> {
                         ),
                       ),
                       const SizedBox(height: 16),
-                      TextField(
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Este campo é obrigatório';
+                          }
+
+                          if (double.parse(value.replaceAll(',', '.')) >= 2.5) {
+                            return 'Altura muito alta';
+                          }
+
+                          if (double.parse(value.replaceAll(',', '.')) <= 1.0) {
+                            return 'Altura muito baixa';
+                          }
+                        },
                         keyboardType: TextInputType.number,
                         inputFormatters: [
                           TextInputMask(
@@ -75,12 +148,12 @@ class _CalculateIMCPageState extends State<CalculateIMCPage> {
                             reverse: false,
                           )
                         ],
-                        controller: alturaController,
+                        controller: heightController,
                         textAlign: TextAlign.center,
                         decoration: const InputDecoration(
-                          suffixText: 'cm',
+                          suffixText: 'm',
                           border: OutlineInputBorder(),
-                          labelText: "Altura (cm)",
+                          labelText: "Altura (m)",
                           hintText: "Exemplo: 1,70",
                           hintStyle: TextStyle(color: Colors.grey),
                         ),
@@ -95,9 +168,17 @@ class _CalculateIMCPageState extends State<CalculateIMCPage> {
                   backgroundColor: Theme.of(context).primaryColor,
                   foregroundColor: Colors.white,
                 ),
-                onPressed: () {},
+                onPressed: _calculate,
                 child: const Text('Calcular'),
               ),
+              const SizedBox(height: 16),
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: infoTextColor ?? Theme.of(context).primaryColor,
+                    fontSize: 20),
+              )
             ],
           ),
         ),
